19713193f47c6eec093ca53e34abe7a33f00dcfb
Elliot Nash <elliotnash@gmail.com>
2024-08-15 13:39:09 -0700

    Remove docs project, rename web -> panel

--- apps/panel/app/page.tsx
+++ apps/panel/app/page.tsx
@@ -46,7 +46,7 @@ export default function Home() {
             Read our docs
           </a>
         </div>
-        <Button appName="docs" className={styles.secondary}>
+        <Button appName="panel" className={styles.secondary}>
           Open alert
         </Button>
       </main>
