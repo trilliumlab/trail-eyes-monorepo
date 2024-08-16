7bf77d7eba65b0313f65f0c479766fccb234d288
Elliot Nash <elliotnash@gmail.com>
2024-08-15 18:15:56 -0700

    Format

--- apps/panel/app/page.tsx
+++ apps/panel/app/page.tsx
@@ -1,6 +1,6 @@
-import { buttonVariants } from "@repo/ui/components/button";
-import { cn } from "@repo/ui/lib/utils";
-import Image from "next/image";
+import { buttonVariants } from '@repo/ui/components/button';
+import { cn } from '@repo/ui/lib/utils';
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -16,7 +16,7 @@ export default function Home() {
         />
         <ol className="font-mono list-inside list-decimal text-sm text-center sm:text-left">
           <li className="mb-2">
-            Get started by editing{" "}
+            Get started by editing{' '}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               app/page.tsx
             </code>
@@ -26,7 +26,10 @@ export default function Home() {
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
-            className={cn(buttonVariants({ variant: "default" }), "gap-2 text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5")}
+            className={cn(
+              buttonVariants({ variant: 'default' }),
+              'gap-2 text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5',
+            )}
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
@@ -41,7 +44,10 @@ export default function Home() {
             Deploy now
           </a>
           <a
-            className={cn(buttonVariants({ variant: "outline" }), 'text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44')}
+            className={cn(
+              buttonVariants({ variant: 'outline' }),
+              'text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44',
+            )}
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
@@ -57,13 +63,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/file-text.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
+          <Image aria-hidden src="/file-text.svg" alt="File icon" width={16} height={16} />
           Learn
         </a>
         <a
@@ -72,13 +72,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
+          <Image aria-hidden src="/window.svg" alt="Window icon" width={16} height={16} />
           Examples
         </a>
         <a
@@ -87,13 +81,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
+          <Image aria-hidden src="/globe.svg" alt="Globe icon" width={16} height={16} />
           Go to nextjs.org →
         </a>
       </footer>
