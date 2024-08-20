74b2eec5e1872b75c65c30c4237236d2749e7ff4
Elliot Nash <elliotnash@gmail.com>
2024-08-20 15:26:02 -0700

    Increase map size

--- apps/panel/app/(panel)/page.tsx
+++ apps/panel/app/(panel)/page.tsx
@@ -27,7 +27,7 @@ export default function Dashboard() {
       <NavBar />
       <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
         <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
-          <Card x-chunk="dashboard-01-chunk-0">
+          <Card>
             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
               <CardTitle className="text-sm font-medium">Total Revenue</CardTitle>
               <DollarSign className="h-4 w-4 text-muted-foreground" />
@@ -37,7 +37,7 @@ export default function Dashboard() {
               <p className="text-xs text-muted-foreground">+20.1% from last month</p>
             </CardContent>
           </Card>
-          <Card x-chunk="dashboard-01-chunk-1">
+          <Card>
             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
               <CardTitle className="text-sm font-medium">Subscriptions</CardTitle>
               <Users className="h-4 w-4 text-muted-foreground" />
@@ -47,7 +47,7 @@ export default function Dashboard() {
               <p className="text-xs text-muted-foreground">+180.1% from last month</p>
             </CardContent>
           </Card>
-          <Card x-chunk="dashboard-01-chunk-2">
+          <Card>
             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
               <CardTitle className="text-sm font-medium">Sales</CardTitle>
               <CreditCard className="h-4 w-4 text-muted-foreground" />
@@ -57,7 +57,7 @@ export default function Dashboard() {
               <p className="text-xs text-muted-foreground">+19% from last month</p>
             </CardContent>
           </Card>
-          <Card x-chunk="dashboard-01-chunk-3">
+          <Card>
             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
               <CardTitle className="text-sm font-medium">Active Now</CardTitle>
               <Activity className="h-4 w-4 text-muted-foreground" />
@@ -69,7 +69,7 @@ export default function Dashboard() {
           </Card>
         </div>
         <div className="grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3">
-          <Card className="xl:col-span-2" x-chunk="dashboard-01-chunk-4">
+          <Card>
             <CardHeader className="flex flex-row items-center">
               <div className="grid gap-2">
                 <CardTitle>Reports</CardTitle>
@@ -188,7 +188,7 @@ export default function Dashboard() {
               </Table>
             </CardContent>
           </Card>
-          <Card className="overflow-clip min-h-[32rem]">
+          <Card className="xl:col-span-2 overflow-clip min-h-[32rem]">
             <TrailEyesMap />
           </Card>
         </div>
