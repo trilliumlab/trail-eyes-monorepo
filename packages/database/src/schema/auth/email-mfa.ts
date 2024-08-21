fcdb31c5d5081ea6ebac8e45ead53ae05443543a
Elliot Nash <elliotnash@gmail.com>
2024-08-21 16:29:07 -0700

    Rename id -> user_id on mfa tables

--- packages/database/src/schema/auth/email-mfa.ts
+++ packages/database/src/schema/auth/email-mfa.ts
@@ -2,7 +2,7 @@ import { pgTable, text } from 'drizzle-orm/pg-core';
 import { users } from './users';
 
 export const emailMfa = pgTable('email_mfa', {
-  id: text('id')
+  userId: text('user_id')
     .references(() => users.id)
     .primaryKey(),
 });
