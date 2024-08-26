5f09e0924a3a5abd90c95f45090ca8ba33d4989c
Elliot Nash <elliotnash@gmail.com>
2024-08-26 13:09:39 -0700

    Animate button scale

--- packages/ui/src/components/ui/button.tsx
+++ packages/ui/src/components/ui/button.tsx
@@ -1,11 +1,14 @@
+'use client';
+
 import { Slot } from '@radix-ui/react-slot';
 import { type VariantProps, cva } from 'class-variance-authority';
 import * as React from 'react';
-
 import { cn } from '@repo/ui/lib/utils';
 
+const animationDuration = 150;
+
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[active=true]:scale-[98%] active:scale-[98%]',
   {
     variants: {
       variant: {
@@ -38,9 +41,32 @@ export interface ButtonProps
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const [animating, setAnimating] = React.useState(false);
+    const timerRef = React.useRef<NodeJS.Timeout | undefined>(undefined);
+
+    // On press, set animation to true for duration of animation
+    function handlePress() {
+      setAnimating(true);
+      timerRef.current = setTimeout(() => {
+        setAnimating(false);
+      }, animationDuration);
+    }
+
+    // Clear timer on unmount
+    React.useEffect(() => () => clearTimeout(timerRef.current), []);
+
     const Comp = asChild ? Slot : 'button';
     return (
-      <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        data-active={animating}
+        ref={ref}
+        {...props}
+        onMouseDown={(e) => {
+          handlePress();
+          props.onMouseDown?.(e);
+        }}
+      />
     );
   },
 );
