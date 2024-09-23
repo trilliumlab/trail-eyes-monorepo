a6f2b7876c9dc2d55f1d4556ebac0f0652f69429
Elliot Nash <elliotnash@gmail.com>
2024-09-22 17:16:20 -0700

    temp remove isomorphic fetch

--- apps/auth/app/backend.ts
+++ apps/auth/app/backend.ts
@@ -1,26 +1,26 @@
 import { initQueryClient } from '@ts-rest/react-query';
 import { contract } from '@repo/contract';
 import { publicEnv } from '@repo/env';
-import { tsRestFetchApi } from '@ts-rest/core';
-import { isServer } from '@tanstack/react-query';
+// import { tsRestFetchApi } from '@ts-rest/core';
+// import { isServer } from '@tanstack/react-query';
 
 export const backend = initQueryClient(contract, {
   baseUrl: publicEnv().backendUrl,
-  api: isServer
-    ? async (args) => {
-        // Lazy import so not imported on the client
-        const { getHeaders, setHeaders } = await import('vinxi/http');
-        // Add request headers from client
-        const response = await tsRestFetchApi({
-          ...args,
-          headers: {
-            ...args.headers,
-            ...getHeaders(),
-          },
-        });
-        // Write response headers to client
-        setHeaders(Object.fromEntries(response.headers));
-        return response;
-      }
-    : undefined,
+  // api: isServer
+  //   ? async (args) => {
+  //       // Lazy import so not imported on the client
+  //       const { getHeaders, setHeaders } = await import('vinxi/http');
+  //       // Add request headers from client
+  //       const response = await tsRestFetchApi({
+  //         ...args,
+  //         headers: {
+  //           ...args.headers,
+  //           ...getHeaders(),
+  //         },
+  //       });
+  //       // Write response headers to client
+  //       setHeaders(Object.fromEntries(response.headers));
+  //       return response;
+  //     }
+  //   : undefined,
 });
