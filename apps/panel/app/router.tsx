62976876913c14781bc8df11322bfd8e30182046
Elliot Nash <elliotnash@gmail.com>
2024-09-27 10:27:47 -0700

    Add posthog to panel

--- apps/panel/app/router.tsx
+++ apps/panel/app/router.tsx
@@ -1,10 +1,22 @@
 import { createRouter as createTanStackRouter } from '@tanstack/react-router';
 import { routeTree } from './routeTree.gen';
+import { posthog } from 'posthog-js';
+import { PostHogProvider } from 'posthog-js/react';
+import { publicEnv } from '@repo/env';
+
+posthog.init(publicEnv().posthogKey, {
+  api_host: publicEnv().posthogHost,
+  person_profiles: 'identified_only',
+  persistence: 'cookie',
+});
 
 export function createRouter() {
   const router = createTanStackRouter({
     routeTree,
     defaultPreload: 'intent',
+    Wrap: ({ children }) => {
+      return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
+    },
   });
 
   return router;
