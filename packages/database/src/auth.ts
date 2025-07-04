83ccec7f34f97945219b90709848354f2453d939
Elliot Nash <elliotnash@gmail.com>
2025-07-04 12:22:51 -0700

    Update emails

--- packages/database/src/auth.ts
+++ packages/database/src/auth.ts
@@ -2,10 +2,21 @@ import { client } from './db-client';
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { openAPI } from "better-auth/plugins"
+import { mailer } from '@repo/email';
 
 export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
+    requireEmailVerification: true,
+  },
+  emailVerification: {
+    sendVerificationEmail: async ( { user, url, token }, request) => {
+      await mailer.sendVerification(user.email, {
+        url,
+        name: user.name,
+        expirationString: '1 hour',
+      });
+    },
   },
   database: drizzleAdapter(client, {
     provider: "pg",
--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -42,6 +42,8 @@ export const PublicEnvSchema = z.object({
 });
 
 export const PrivateEnvSchema = z.object({
+  // packages/auth
+  BETTER_AUTH_SECRET: z.string(),
   // packages/database
   DB_NAME: z.string(),
   DB_HOST: z.string(),
