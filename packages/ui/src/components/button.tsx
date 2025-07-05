d61520067eca9c0b55e41be303ea561e3c63f7c9
Elliot Nash <elliotnash@gmail.com>
2025-07-05 09:06:45 -0700

    Tailwind V4 migration pt 1

--- packages/ui/src/components/button.tsx
+++ packages/ui/src/components/button.tsx
@@ -8,7 +8,7 @@ import { cn } from '~/lib/utils';
 const animationDuration = 150;
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[active=true]:scale-[98%] active:scale-[98%]',
+  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-all focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[active=true]:scale-[98%] active:scale-[98%]',
   {
     variants: {
       variant: {
@@ -23,7 +23,7 @@ const buttonVariants = cva(
         default: 'h-10 px-4 py-2',
         sm: 'h-9 rounded-md px-3',
         lg: 'h-11 rounded-md px-8',
-        icon: 'h-10 w-10',
+        icon: 'size-10',
       },
     },
     defaultVariants: {
--- packages/ui/src/components/link.tsx
+++ packages/ui/src/components/link.tsx
@@ -2,7 +2,7 @@ import { Link as TSLink, type LinkComponent as TSLinkComponent } from '@tanstack
 import { cn } from '~/lib/utils';
 
 export const linkStyle =
-  'cursor-pointer transition-colors rounded-sm hover:text-muted-foreground focus-visible:outline-none focus-visible:ring-offset-background focus-visible:ring-offset-1 focus-visible:ring-2 focus-visible:ring-ring underline';
+  'cursor-pointer transition-colors rounded-sm hover:text-muted-foreground focus-visible:outline-hidden focus-visible:ring-offset-background focus-visible:ring-offset-1 focus-visible:ring-2 focus-visible:ring-ring underline';
 
 export const Link: TSLinkComponent<'a'> = ({ className, ...props }) => {
   return <TSLink {...props} className={cn(linkStyle, className)} />;
