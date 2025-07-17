2b1b1e194b4cf1ff5498e42f4f14a72936e003c6
Elliot Nash <elliotnash@gmail.com>
2025-07-17 14:28:46 -0700

    Remove commented type

--- apps/backend/src/routes/geojson.ts
+++ apps/backend/src/routes/geojson.ts
@@ -35,29 +35,6 @@ const routesJsonMemo = memoize(
   },
 );
 
-/*
-
-(parameter) input: {
-    creatorDeviceId: string;
-    category: "other" | "fallenTree" | "drainage" | "erosion" | "structureFailure" | "damagedSign" | "seasonal";
-    route: number;
-    trail: number;
-    geometry: {
-        coordinates: [number, number, number];
-        type: "Point";
-    };
-    id?: number | undefined;
-    localId?: string | null | undefined;
-    creatorUserId?: string | null | undefined;
-    image?: string | null | undefined;
-    blurHash?: string | null | undefined;
-    status?: "open" | "confirmed" | "inProgress" | "closed" | undefined;
-    reportedAt?: Date | undefined;
-    updatedAt?: Date | undefined;
-}
-
-*/
-
 const reportsJsonMemo = memoize(
   async () => {
     const reports = await db.getAllReports();
