6d881fe79370ce77bae7c0adc31ee6ca7dafb75c
stacie-ganguly <stacieg529@gmail.com>
2025-08-04 12:31:37 -0700

    Update reports.ts

--- apps/backend/src/routes/reports.ts
+++ apps/backend/src/routes/reports.ts
@@ -1,11 +1,20 @@
 import { db } from '@repo/database';
 import { pub } from '../orpc';
 
-
+/*
 export const postReport = pub.reports.postReport.handler(async ({input, context}) => {
   return await db.addReport(input);
 });
+*/
 
+export const postReport = pub.reports.postReport.handler(async ({input, context}) => {
+  const savedReport = await db.addReport(input);
+
+  return {
+    status:"created",
+    report: savedReport ?? input,
+  };
+});
 export const reportsRouter = {
   postReport: postReport,
 };
